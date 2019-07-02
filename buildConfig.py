1f9b6972521437fe4e3e98604c41f9a5f551200d
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-07-02 14:47:57 -0600

    buildConfig, runTest: handle more of the testset configuration

--- buildConfig.py
+++ buildConfig.py
@@ -7,59 +7,71 @@ from util import *
 from kast import *
 from pyk  import *
 
-ALL_symbols = K_symbols
+BEACON_CHAIN_LISTS = [ "PendingAttestation"
+                     , "Bytes32"
+                     , "Uint64"
+                     , "Eth1Data"
+                     ]
+
+BEACON_CHAIN_symbols = { }
+
+for list_sort in BEACON_CHAIN_LISTS:
+    BEACON_CHAIN_symbols["." + list_sort + "List"] = constLabel("")
+    BEACON_CHAIN_symbols[      list_sort + "List"] = underbarUnparsing("__")
+
+ALL_symbols = combineDicts(K_symbols, BEACON_CHAIN_symbols)
 
 # Read in the symbolic configuration
 # configuration = readKastTerm(sys.argv[1])
 
 symbolic_configuration = KApply ( '<beacon-chain>' , [ KApply ( '<k>', [ KVariable('K_CELL') ] )
-                                                   , KApply ( '<state>' , [ KApply ( '<slot>'                        , [ KVariable('SLOT_CELL')                       ] )
-                                                                          , KApply ( '<genesis-time>'                , [ KVariable('GENESIS_TIME_CELL')               ] )
-                                                                          , KApply ( '<fork>'                        , [ KVariable('FORK_CELL')                       ] )
-                                                                          , KApply ( '<validator-registry>'          , [ KVariable('VALIDATOR_REGISTRY_CELL')         ] )
-                                                                          , KApply ( '<balances>'                    , [ KVariable('BALANCES_CELL')                   ] )
-                                                                          , KApply ( '<latest-randao-mixes>'         , [ KVariable('LATEST_RANDAO_MIXES_CELL')        ] )
-                                                                          , KApply ( '<latest-start-shard>'          , [ KVariable('LATEST_START_SHARD_CELL')         ] )
-                                                                          , KApply ( '<previous-epoch-attestations>' , [ KVariable('PREVIOUS_EPOCH_ATTESTATION_CELL') ] )
-                                                                          , KApply ( '<current-epoch-attestations>'  , [ KVariable('CURRENT_EPOCH_ATTESTATIONS_CELL') ] )
-                                                                          , KApply ( '<previous-justified-epoch>'    , [ KVariable('PREVIOUS_JUSTIFIED_EPOCH_CELL')   ] )
-                                                                          , KApply ( '<current-justified-epoch>'     , [ KVariable('CURRENT_JUSTIFIED_EPOCH_CELL')    ] )
-                                                                          , KApply ( '<previous-justified-root>'     , [ KVariable('PREVIOUS_JUSTIFIED_ROOT_CELL')    ] )
-                                                                          , KApply ( '<current-justified-root>'      , [ KVariable('CURRENT_JUSTIFIED_ROOT_CELL')     ] )
-                                                                          , KApply ( '<justification-bitfield>'      , [ KVariable('JUSTIFICATION_BITFIELD_CELL')     ] )
-                                                                          , KApply ( '<finalized-epoch>'             , [ KVariable('FINALIZED_EPOCH_CELL')            ] )
-                                                                          , KApply ( '<finalized-root>'              , [ KVariable('FINALIZED_ROOT_CELL')             ] )
-                                                                          , KApply ( '<current-crosslinks>'          , [ KVariable('CURRENT_CROSSLINKS_CELL')         ] )
-                                                                          , KApply ( '<previous-crosslinks>'         , [ KVariable('PREVIOUS_CROSSLINKS_CELL')        ] )
-                                                                          , KApply ( '<latest-block-roots>'          , [ KVariable('LATEST_BLOCK_ROOTS_CELL')         ] )
-                                                                          , KApply ( '<latest-state-roots>'          , [ KVariable('LATEST_STATE_ROOTS_CELL')         ] )
-                                                                          , KApply ( '<latest-active-index-roots>'   , [ KVariable('LATEST_ACTIVE_INDEX_ROOTS_CELL')  ] )
-                                                                          , KApply ( '<latest-slashed-balances>'     , [ KVariable('LATEST_SLASHED_BALANCE_CELL')     ] )
-                                                                          , KApply ( '<latest-block-header>'         , [ KVariable('LATEST_BLOCK_HEADER_CELL')        ] )
-                                                                          , KApply ( '<historical-roots>'            , [ KVariable('HISTORICAL_ROOTS_CELL')           ] )
-                                                                          , KApply ( '<latest-eth1-data>'            , [ KVariable('LATEST_ETH1_DATA_CELL')           ] )
-                                                                          , KApply ( '<eth1-data-votes>'             , [ KVariable('ETH1_DATA_VOTES_CELL')            ] )
-                                                                          , KApply ( '<deposit-index>'               , [ KVariable('DEPOSIT_INDEX_CELL')              ] )
-                                                                          ]
-                                                            )
-                                                   , KApply ( '<block>' , [ KApply ( '<blockSlot>'   , [ KVariable('BLOCKSLOT_CELL')   ] )
-                                                                          , KApply ( '<parent-root>' , [ KVariable('PARENT_ROOT_CELL') ] )
-                                                                          , KApply ( '<state-root>'  , [ KVariable('STATE_ROOT_CELL')  ] )
-                                                                          , KApply ( '<body>' , [ KApply ( '<randao-reveal>'      , [ KVariable('RANDAO_REVEAL_CELL')      ] )
-                                                                                                , KApply ( '<eth1-data>'          , [ KVariable('ETH1_DATA_CELL')          ] )
-                                                                                                , KApply ( '<graffiti>'           , [ KVariable('GRAFFITI_CELL')           ] )
-                                                                                                , KApply ( '<proposer-slashings>' , [ KVariable('PROPOSER_SLASHINGS_CELL') ] )
-                                                                                                , KApply ( '<attester-slashings>' , [ KVariable('ATTESTER_SLASHINGS_CELL') ] )
-                                                                                                , KApply ( '<attestations>'       , [ KVariable('ATTESTATIONS_CELL')       ] )
-                                                                                                , KApply ( '<deposits>'           , [ KVariable('DEPOSITS_CELL')           ] )
-                                                                                                , KApply ( '<voluntary-exits>'    , [ KVariable('VOLUNTARY_EXITS_CELL')    ] )
-                                                                                                , KApply ( '<transfers>'          , [ KVariable('TRANSFERS_CELL')          ] )
-                                                                                                ]
-                                                                                   )
-                                                                           , KApply ( '<signature>', [ KVariable('SIGNATURE_CELL') ] )
-                                                                           ]
-                                                            )
-                                                   ]
+                                                     , KApply ( '<state>' , [ KApply ( '<slot>'                        , [ KVariable('SLOT_CELL')                       ] )
+                                                                            , KApply ( '<genesis-time>'                , [ KVariable('GENESIS_TIME_CELL')               ] )
+                                                                            , KApply ( '<fork>'                        , [ KVariable('FORK_CELL')                       ] )
+                                                                            , KApply ( '<validator-registry>'          , [ KVariable('VALIDATOR_REGISTRY_CELL')         ] )
+                                                                            , KApply ( '<balances>'                    , [ KVariable('BALANCES_CELL')                   ] )
+                                                                            , KApply ( '<latest-randao-mixes>'         , [ KVariable('LATEST_RANDAO_MIXES_CELL')        ] )
+                                                                            , KApply ( '<latest-start-shard>'          , [ KVariable('LATEST_START_SHARD_CELL')         ] )
+                                                                            , KApply ( '<previous-epoch-attestations>' , [ KVariable('PREVIOUS_EPOCH_ATTESTATION_CELL') ] )
+                                                                            , KApply ( '<current-epoch-attestations>'  , [ KVariable('CURRENT_EPOCH_ATTESTATIONS_CELL') ] )
+                                                                            , KApply ( '<previous-justified-epoch>'    , [ KVariable('PREVIOUS_JUSTIFIED_EPOCH_CELL')   ] )
+                                                                            , KApply ( '<current-justified-epoch>'     , [ KVariable('CURRENT_JUSTIFIED_EPOCH_CELL')    ] )
+                                                                            , KApply ( '<previous-justified-root>'     , [ KVariable('PREVIOUS_JUSTIFIED_ROOT_CELL')    ] )
+                                                                            , KApply ( '<current-justified-root>'      , [ KVariable('CURRENT_JUSTIFIED_ROOT_CELL')     ] )
+                                                                            , KApply ( '<justification-bitfield>'      , [ KVariable('JUSTIFICATION_BITFIELD_CELL')     ] )
+                                                                            , KApply ( '<finalized-epoch>'             , [ KVariable('FINALIZED_EPOCH_CELL')            ] )
+                                                                            , KApply ( '<finalized-root>'              , [ KVariable('FINALIZED_ROOT_CELL')             ] )
+                                                                            , KApply ( '<current-crosslinks>'          , [ KVariable('CURRENT_CROSSLINKS_CELL')         ] )
+                                                                            , KApply ( '<previous-crosslinks>'         , [ KVariable('PREVIOUS_CROSSLINKS_CELL')        ] )
+                                                                            , KApply ( '<latest-block-roots>'          , [ KVariable('LATEST_BLOCK_ROOTS_CELL')         ] )
+                                                                            , KApply ( '<latest-state-roots>'          , [ KVariable('LATEST_STATE_ROOTS_CELL')         ] )
+                                                                            , KApply ( '<latest-active-index-roots>'   , [ KVariable('LATEST_ACTIVE_INDEX_ROOTS_CELL')  ] )
+                                                                            , KApply ( '<latest-slashed-balances>'     , [ KVariable('LATEST_SLASHED_BALANCE_CELL')     ] )
+                                                                            , KApply ( '<latest-block-header>'         , [ KVariable('LATEST_BLOCK_HEADER_CELL')        ] )
+                                                                            , KApply ( '<historical-roots>'            , [ KVariable('HISTORICAL_ROOTS_CELL')           ] )
+                                                                            , KApply ( '<latest-eth1-data>'            , [ KVariable('LATEST_ETH1_DATA_CELL')           ] )
+                                                                            , KApply ( '<eth1-data-votes>'             , [ KVariable('ETH1_DATA_VOTES_CELL')            ] )
+                                                                            , KApply ( '<deposit-index>'               , [ KVariable('DEPOSIT_INDEX_CELL')              ] )
+                                                                            ]
+                                                              )
+                                                     , KApply ( '<block>' , [ KApply ( '<blockSlot>'   , [ KVariable('BLOCKSLOT_CELL')   ] )
+                                                                            , KApply ( '<parent-root>' , [ KVariable('PARENT_ROOT_CELL') ] )
+                                                                            , KApply ( '<state-root>'  , [ KVariable('STATE_ROOT_CELL')  ] )
+                                                                            , KApply ( '<body>' , [ KApply ( '<randao-reveal>'      , [ KVariable('RANDAO_REVEAL_CELL')      ] )
+                                                                                                  , KApply ( '<eth1-data>'          , [ KVariable('ETH1_DATA_CELL')          ] )
+                                                                                                  , KApply ( '<graffiti>'           , [ KVariable('GRAFFITI_CELL')           ] )
+                                                                                                  , KApply ( '<proposer-slashings>' , [ KVariable('PROPOSER_SLASHINGS_CELL') ] )
+                                                                                                  , KApply ( '<attester-slashings>' , [ KVariable('ATTESTER_SLASHINGS_CELL') ] )
+                                                                                                  , KApply ( '<attestations>'       , [ KVariable('ATTESTATIONS_CELL')       ] )
+                                                                                                  , KApply ( '<deposits>'           , [ KVariable('DEPOSITS_CELL')           ] )
+                                                                                                  , KApply ( '<voluntary-exits>'    , [ KVariable('VOLUNTARY_EXITS_CELL')    ] )
+                                                                                                  , KApply ( '<transfers>'          , [ KVariable('TRANSFERS_CELL')          ] )
+                                                                                                  ]
+                                                                                     )
+                                                                             , KApply ( '<signature>', [ KVariable('SIGNATURE_CELL') ] )
+                                                                             ]
+                                                              )
+                                                     ]
                                 )
 
 init_cells = { 'K_CELL'                          : KToken('.Pgm', 'Pgm')
--- runTest.py
+++ runTest.py
@@ -3,46 +3,71 @@
 import sys
 import yaml
 
+from functools import reduce
+
 from buildConfig import *
 
-intToken = lambda x: KToken(str(x), 'Int')
+intToken    = lambda x: KToken(str(x), 'Int')
+stringToken = lambda x: KToken('"' + str(x) + '"', 'String')
 
-def consList(join, empty = ""):
-    def _consList(input):
-        if len(input) == 0:
-            return KToken(empty, "K")
-        else:
-            return KToken(str(input), "NOT_IMPLEMENTED")
-    return _consList
+hexIntToken = lambda x: intToken(int(x, 16))
 
-pre_keys = { "slot"                        : ('SLOT_CELL'         , intToken)
-           , "genesis_time"                : ('GENESIS_TIME_CELL' , intToken)
-           # , "fork"                        : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "validator_registry"          : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "balances"                    : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "latest_randao_mixes"         : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           , "latest_start_shard"          : ('LATEST_START_SHARD_CELL' , intToken)
-           , "previous_epoch_attestations" : ('PREVIOUS_EPOCH_ATTESTATION_CELL' , consList('???'))
-           , "current_epoch_attestations"  : ('CURRENT_EPOCH_ATTESTATIONS_CELL' , consList('???'))
-           , "previous_justified_epoch"    : ('PREVIOUS_JUSTIFIED_EPOCH_CELL' , intToken)
-           , "current_justified_epoch"     : ('CURRENT_JUSTIFIED_EPOCH_CELL' , intToken)
-           # , "previous_justified_root"     : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "current_justified_root"      : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           , "justification_bitfield"      : ('JUSTIFICATION_BITFIELD_CELL' , intToken)
-           , "finalized_epoch"             : ('FINALIZED_EPOCH_CELL' , intToken)
-           # , "finalized_root"              : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "current_crosslinks"          : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "previous_crosslinks"         : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "latest_block_roots"          : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "latest_state_roots"          : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "latest_active_index_roots"   : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "latest_slashed_balances"     : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           # , "latest_block_header"         : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           , "historical_roots"            : ('HISTORICAL_ROOTS_CELL' , consList('???'))
-           # , "latest_eth1_data"            : ('NOT_IMPLEMENTED' , lambda x: KVariable('NOT_IMPLEMENTED'))
-           , "eth1_data_votes"             : ('ETH1_DATA_VOTES_CELL' , consList('???'))
-           , "deposit_index"               : ('DEPOSIT_INDEX_CELL' , intToken)
+unimplemented = lambda input: KToken("UNIMPLEMENTED { " + str(input) + "}", "K")
+
+foldr = lambda func, init: lambda xs: reduce(lambda x, y: func(y, x), xs[::-1], init)
+
+def assocWithUnitAST(joinKLabel, emptyKLabel, converter = lambda x: x):
+    emptyElem = KApply(emptyKLabel, [])
+    _join = lambda head, tail: KApply(joinKLabel, [converter(head), tail])
+    return foldr(_join, KApply(emptyKLabel, []))
+
+def indexedMapOf(mapElement):
+    def _indexedMapOf(inputList):
+        mapElements = [ KApply("_|->_", [intToken(k), mapElement(v)]) for (k,v) in enumerate(inputList) ]
+        return assocWithUnitAST("_Map_", ".Map")(mapElements)
+    return _indexedMapOf
+
+def listOf(sort, converter = lambda x: x):
+    listSort = sort + "List"
+    listUnit = "." + listSort
+    return assocWithUnitAST(listSort, listUnit, converter = converter)
+
+pre_keys = { "slot"                        : ('SLOT_CELL'                       , intToken)
+           , "genesis_time"                : ('GENESIS_TIME_CELL'               , intToken)
+           , "fork"                        : ('FORK_CELL'                       , unimplemented)
+           , "validator_registry"          : ('VALIDATOR_REGISTRY_CELL'         , indexedMapOf(unimplemented))
+           , "balances"                    : ('BALANCES_CELL'                   , indexedMapOf(intToken))
+           , "latest_randao_mixes"         : ('LATEST_RANDAO_MIXES_CELL'        , listOf("Bytes32", converter = hexIntToken))
+           , "latest_start_shard"          : ('LATEST_START_SHARD_CELL'         , intToken)
+           , "previous_epoch_attestations" : ('PREVIOUS_EPOCH_ATTESTATION_CELL' , listOf("PendingAttestation", converter = unimplemented))
+           , "current_epoch_attestations"  : ('CURRENT_EPOCH_ATTESTATIONS_CELL' , listOf("PendingAttestation", converter = unimplemented))
+           , "previous_justified_epoch"    : ('PREVIOUS_JUSTIFIED_EPOCH_CELL'   , intToken)
+           , "current_justified_epoch"     : ('CURRENT_JUSTIFIED_EPOCH_CELL'    , intToken)
+           , "previous_justified_root"     : ('PREVIOUS_JUSTIFIED_ROOT_CELL'    , hexIntToken)
+           , "current_justified_root"      : ('CURRENT_JUSTIFIED_ROOT_CELL'     , hexIntToken)
+           , "justification_bitfield"      : ('JUSTIFICATION_BITFIELD_CELL'     , intToken)
+           , "finalized_epoch"             : ('FINALIZED_EPOCH_CELL'            , intToken)
+           , "finalized_root"              : ('FINALIZED_ROOT_CELL'             , hexIntToken)
+           , "current_crosslinks"          : ('CURRENT_CROSSLINKS_CELL'         , unimplemented)
+           , "previous_crosslinks"         : ('PREVIOUS_CROSSLINKS_CELL'        , unimplemented)
+           , "latest_block_roots"          : ('LATEST_BLOCK_ROOTS_CELL'         , listOf("Bytes32", converter = hexIntToken))
+           , "latest_state_roots"          : ('LATEST_STATE_ROOTS_CELL'         , listOf("Bytes32", converter = hexIntToken))
+           , "latest_active_index_roots"   : ('LATEST_ACTIVE_INDEX_ROOTS_CELL'  , listOf("Bytes32", converter = hexIntToken))
+           , "latest_slashed_balances"     : ('LATEST_SLASHED_BALANCE_CELL'     , listOf("Uint64", converter = intToken))
+           , "latest_block_header"         : ('LATEST_BLOCK_HEADER_CELL'        , unimplemented)
+           , "historical_roots"            : ('HISTORICAL_ROOTS_CELL'           , listOf("Bytes32", converter = intToken))
+           , "latest_eth1_data"            : ('LATEST_ETH1_DATA_CELL'           , unimplemented)
+           , "eth1_data_votes"             : ('ETH1_DATA_VOTES_CELL'            , listOf("Eth1Data", converter = unimplemented))
+           , "deposit_index"               : ('DEPOSIT_INDEX_CELL'              , intToken)
            }
+
+def emptyKLabelsToEmptyTokens(k):
+    _emptyKLabelsToEmptyTokens = [ (KApply(".Map", []), KToken("", "Map"))
+                                 ]
+    newK = k
+    for rule in _emptyKLabelsToEmptyTokens:
+        newK = rewriteAnywhereWith(rule, newK)
+    return newK
 
 if __name__ == "__main__":
     test_file = sys.argv[1]
@@ -65,6 +90,7 @@ if __name__ == "__main__":
         if pre_key in pre_keys:
             (cell_var, converter) = pre_keys[pre_key]
             if cell_var in keytable:
+                print(pre_key)
                 keytable[cell_var] = converter(test_pre)
             else:
                 print("unimplemented pre-key: " + str(pre_key) + " -> " + str(test_pre))
@@ -76,5 +102,5 @@ if __name__ == "__main__":
     else:
         print("post keys: " + str(test_case['post'].keys()))
 
-    init_config = substitute(symbolic_configuration, keytable)
+    init_config = emptyKLabelsToEmptyTokens(substitute(symbolic_configuration, keytable))
     print(prettyPrintKast(init_config, ALL_symbols))

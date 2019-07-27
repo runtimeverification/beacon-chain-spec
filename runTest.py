92de93aebe75cca07921fa2c6ac3d30163d01dc3
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-07-27 13:55:44 -0600

    runTest: better reporting of unset configuration parameters

--- runTest.py
+++ runTest.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import copy
 import json
 import sys
 import tempfile
@@ -67,45 +66,48 @@ pendingAttestationTerm = labelWithKeyPairs("#PendingAttestation" , [ ('aggregati
                                                                    ]
                                           )
 
-pre_keys = { "genesis_time"                  : ('GENESIS_TIME_CELL'               , intToken)
-           , "slot"                          : ('SLOT_CELL'                       , intToken)
-           , "fork"                          : ('FORK_CELL'                       , forkTerm)
-           , "latest_block_header"           : ('LATEST_BLOCK_HEADER_CELL'        , blockheaderTerm)
-           , "block_roots"                   : ('BLOCK_ROOTS_CELL'                , indexedMapOf(converter = hexIntToken))
-           , "state_roots"                   : ('STATE_ROOTS_CELL'                , indexedMapOf(converter = hexIntToken))
-           , "historical_roots"              : ('HISTORICAL_ROOTS_CELL'           , listOf("Hash", converter = hexIntToken))
-           , "eth1_data"                     : ('ETH1_DATA_CELL'                  , eth1dataTerm)
-           , "eth1_data_votes"               : ('ETH1_DATA_VOTES_CELL'            , listOf("Eth1Data", converter = eth1dataTerm))
-           , "eth1_deposit_index"            : ('ETH1_DEPOSIT_INDEX_CELL'         , intToken)
-           , "validators"                    : ('VALIDATORS_CELL'                 , indexedMapOf(converter = validatorTerm))
-           , "balances"                      : ('BALANCES_CELL'                   , indexedMapOf(converter = intToken))
-           , "start_shard"                   : ('START_SHARD_CELL'                , intToken)
-           , "randao_mixes"                  : ('RANDAO_MIXES_CELL'               , listOf("Hash", converter = hexIntToken))
-           , "active_index_roots"            : ('ACTIVE_INDEX_ROOTS_CELL'         , listOf("Hash", converter = hexIntToken))
-           , "compact_committees_roots"      : ('COMPACT_COMMITTEES_ROOTS'        , listOf("Hash", converter = hexIntToken))
-           , "slashings"                     : ('SLASHINGS'                       , indexedMapOf(converter = intToken))
-           , "previous_epoch_attestations"   : ('PREVIOUS_EPOCH_ATTESTATION_CELL' , listOf("PendingAttestation", converter = pendingAttestationTerm))
-           , "current_epoch_attestations"    : ('CURRENT_EPOCH_ATTESTATIONS_CELL' , listOf("PendingAttestation", converter = pendingAttestationTerm))
-           , "previous_crosslinks"           : ('PREVIOUS_CROSSLINKS_CELL'        , indexedMapOf(converter = crosslinkTerm))
-           , "current_crosslinks"            : ('CURRENT_CROSSLINKS_CELL'         , indexedMapOf(converter = crosslinkTerm))
-           , "justification_bits"            : ('JUSTIFICATION_BITS'              , listOf("Uint64", converter = intToken))
-           , "previous_justified_checkpoint" : ('PREVIOUS_JUSTIFIED_CHECKPOINT'   , checkpointTerm)
-           , "current_justified_checkpoint"  : ('CURRENT_JUSTIFIED_CHECKPOINT'    , checkpointTerm)
-           , "finalized_checkpoint"          : ('FINALIZED_CHECKPOINT'            , checkpointTerm)
+pre_keys = { "genesis_time"                  : ('GENESIS_TIME_CELL'                  , intToken)
+           , "slot"                          : ('SLOT_CELL'                          , intToken)
+           , "fork"                          : ('FORK_CELL'                          , forkTerm)
+           , "latest_block_header"           : ('LATEST_BLOCK_HEADER_CELL'           , blockheaderTerm)
+           , "block_roots"                   : ('BLOCK_ROOTS_CELL'                   , indexedMapOf(converter = hexIntToken))
+           , "state_roots"                   : ('STATE_ROOTS_CELL'                   , indexedMapOf(converter = hexIntToken))
+           , "historical_roots"              : ('HISTORICAL_ROOTS_CELL'              , listOf("Hash", converter = hexIntToken))
+           , "eth1_data"                     : ('ETH1_DATA_CELL'                     , eth1dataTerm)
+           , "eth1_data_votes"               : ('ETH1_DATA_VOTES_CELL'               , listOf("Eth1Data", converter = eth1dataTerm))
+           , "deposit_index"                 : ('ETH1_DEPOSIT_INDEX_CELL'            , intToken)
+           , "validator_registry"            : ('VALIDATORS_CELL'                    , indexedMapOf(converter = validatorTerm))
+           , "balances"                      : ('BALANCES_CELL'                      , indexedMapOf(converter = intToken))
+           , "latest_start_shard"            : ('START_SHARD_CELL'                   , intToken)
+           , "randao_mixes"                  : ('RANDAO_MIXES_CELL'                  , listOf("Hash", converter = hexIntToken))
+           , "active_index_roots"            : ('ACTIVE_INDEX_ROOTS_CELL'            , listOf("Hash", converter = hexIntToken))
+           , "compact_committees_roots"      : ('COMPACT_COMMITTEES_ROOTS_CELL'      , listOf("Hash", converter = hexIntToken))
+           , "slashings"                     : ('SLASHINGS_CELL'                     , indexedMapOf(converter = intToken))
+           , "previous_epoch_attestations"   : ('PREVIOUS_EPOCH_ATTESTATION_CELL'    , listOf("PendingAttestation", converter = pendingAttestationTerm))
+           , "current_epoch_attestations"    : ('CURRENT_EPOCH_ATTESTATIONS_CELL'    , listOf("PendingAttestation", converter = pendingAttestationTerm))
+           , "previous_crosslinks"           : ('PREVIOUS_CROSSLINKS_CELL'           , indexedMapOf(converter = crosslinkTerm))
+           , "current_crosslinks"            : ('CURRENT_CROSSLINKS_CELL'            , indexedMapOf(converter = crosslinkTerm))
+           , "justification_bitfield"        : ('JUSTIFICATION_BITS_CELL'            , intToken)
+           , "previous_justified_checkpoint" : ('PREVIOUS_JUSTIFIED_CHECKPOINT_CELL' , checkpointTerm)
+           , "current_justified_checkpoint"  : ('CURRENT_JUSTIFIED_CHECKPOINT_CELL'  , checkpointTerm)
+           , "finalized_checkpoint"          : ('FINALIZED_CHECKPOINT_CELL'          , checkpointTerm)
            }
 
 def buildInitConfigSubstitution(test_pre_state, key_table = init_cells, skip_keys = []):
-    new_key_table = copy.deepcopy(key_table)
+    new_key_table = {}
     for pre_key in test_pre_state.keys():
         test_pre = test_pre_state[pre_key]
         if pre_key in pre_keys:
             (cell_var, converter) = pre_keys[pre_key]
-            if cell_var in new_key_table and cell_var not in skip_keys:
+            if cell_var in key_table and cell_var not in skip_keys:
                 new_key_table[cell_var] = converter(test_pre)
             else:
                 printerr("unimplemented pre-key: " + str(pre_key) + " -> " + str(test_pre))
-        elif len(str(test_pre)) < 3:
+        else:
             printerr("unused pre-key: " + pre_key)
+    for cell_var in set(key_table.keys()).difference(set(new_key_table.keys())):
+        printerr("unset configuration variable: " + str(cell_var))
+        new_key_table[cell_var] = key_table[cell_var]
     return new_key_table
 
 if __name__ == "__main__":
@@ -141,7 +143,7 @@ if __name__ == "__main__":
                     , "term"    : substitute(symbolic_configuration, init_config_subst)
                     }
 
-        with tempfile.NamedTemporaryFile(mode = "w", delete = False) as tempf:
+        with tempfile.NamedTemporaryFile(mode = "w") as tempf:
             json.dump(kast_json, tempf)
             tempf.flush()
             (returnCode, _, _) = krun(tempf.name, "--term", "--parser", "cat")

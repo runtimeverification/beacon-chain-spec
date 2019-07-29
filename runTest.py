a84baf793c349c36f3e57a4eb29b5c28444ac095
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2019-07-28 22:50:28 -0600

    runTest: handle <block> sub-cells

--- runTest.py
+++ runTest.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
+import copy
 import difflib
 import json
 import sys
 import tempfile
 import yaml
 
-from pyk.kast    import _fatal, _warning
+from pyk.kast    import _notif, _warning, _fatal
 from buildConfig import *
 
 forkTerm = labelWithKeyPairs('#Fork' , [ ('previous_version' , hexIntToken)
@@ -68,49 +69,73 @@ pendingAttestationTerm = labelWithKeyPairs('#PendingAttestation' , [ ('aggregati
                                                                    ]
                                           )
 
-init_config_cells = { 'GENESIS_TIME_CELL'                  : ('genesis_time'                  , intToken)
-                    , 'SLOT_CELL'                          : ('slot'                          , intToken)
-                    , 'FORK_CELL'                          : ('fork'                          , forkTerm)
-                    , 'LATEST_BLOCK_HEADER_CELL'           : ('latest_block_header'           , blockheaderTerm)
-                    , 'BLOCK_ROOTS_CELL'                   : ('block_roots'                   , indexedMapOf(converter = hexIntToken))
-                    , 'STATE_ROOTS_CELL'                   : ('state_roots'                   , indexedMapOf(converter = hexIntToken))
-                    , 'HISTORICAL_ROOTS_CELL'              : ('historical_roots'              , listOf('Hash', converter = hexIntToken))
-                    , 'ETH1_DATA_CELL'                     : ('latest_eth1_data'              , eth1dataTerm)
-                    , 'ETH1_DATA_VOTES_CELL'               : ('eth1_data_votes'               , listOf('Eth1Data', converter = eth1dataTerm))
-                    , 'ETH1_DEPOSIT_INDEX_CELL'            : ('deposit_index'                 , intToken)
-                    , 'VALIDATORS_CELL'                    : ('validator_registry'            , indexedMapOf(converter = validatorTerm))
-                    , 'BALANCES_CELL'                      : ('balances'                      , indexedMapOf(converter = intToken))
-                    , 'START_SHARD_CELL'                   : ('latest_start_shard'            , intToken)
-                    , 'RANDAO_MIXES_CELL'                  : ('randao_mixes'                  , listOf('Hash', converter = hexIntToken))
-                    , 'ACTIVE_INDEX_ROOTS_CELL'            : ('active_index_roots'            , listOf('Hash', converter = hexIntToken))
-                    , 'COMPACT_COMMITTEES_ROOTS_CELL'      : ('compact_committees_roots'      , listOf('Hash', converter = hexIntToken))
-                    , 'SLASHINGS_CELL'                     : ('slashings'                     , indexedMapOf(converter = intToken))
-                    , 'PREVIOUS_EPOCH_ATTESTATION_CELL'    : ('previous_epoch_attestations'   , listOf('PendingAttestation', converter = pendingAttestationTerm))
-                    , 'CURRENT_EPOCH_ATTESTATIONS_CELL'    : ('current_epoch_attestations'    , listOf('PendingAttestation', converter = pendingAttestationTerm))
-                    , 'PREVIOUS_CROSSLINKS_CELL'           : ('previous_crosslinks'           , indexedMapOf(converter = crosslinkTerm))
-                    , 'CURRENT_CROSSLINKS_CELL'            : ('current_crosslinks'            , indexedMapOf(converter = crosslinkTerm))
-                    , 'JUSTIFICATION_BITS_CELL'            : ('justification_bitfield'        , lambda inputInt: listOf('Bit', converter = intToken)([i for i in bin(int(inputInt))[2:]]))
-                    , 'PREVIOUS_JUSTIFIED_CHECKPOINT_CELL' : ('previous_justified_checkpoint' , checkpointTerm)
-                    , 'CURRENT_JUSTIFIED_CHECKPOINT_CELL'  : ('current_justified_checkpoint'  , checkpointTerm)
-                    , 'FINALIZED_CHECKPOINT_CELL'          : ('finalized_checkpoint'          , checkpointTerm)
+init_config_cells = { 'GENESIS_TIME_CELL'                  : (['genesis_time']                       , intToken)
+                    , 'SLOT_CELL'                          : (['slot']                               , intToken)
+                    , 'FORK_CELL'                          : (['fork']                               , forkTerm)
+                    , 'LATEST_BLOCK_HEADER_CELL'           : (['latest_block_header']                , blockheaderTerm)
+                    , 'BLOCK_ROOTS_CELL'                   : (['block_roots']                        , indexedMapOf(converter = hexIntToken))
+                    , 'STATE_ROOTS_CELL'                   : (['state_roots']                        , indexedMapOf(converter = hexIntToken))
+                    , 'HISTORICAL_ROOTS_CELL'              : (['historical_roots']                   , listOf('Hash', converter = hexIntToken))
+                    , 'ETH1_DATA_CELL'                     : (['eth1_data']                          , eth1dataTerm)
+                    , 'ETH1_DATA_VOTES_CELL'               : (['eth1_data_votes']                    , listOf('Eth1Data', converter = eth1dataTerm))
+                    , 'ETH1_DEPOSIT_INDEX_CELL'            : (['eth1_deposit_index']                 , intToken)
+                    , 'VALIDATORS_CELL'                    : (['validators']                         , indexedMapOf(converter = validatorTerm))
+                    , 'BALANCES_CELL'                      : (['balances']                           , indexedMapOf(converter = intToken))
+                    , 'START_SHARD_CELL'                   : (['start_shard']                        , intToken)
+                    , 'RANDAO_MIXES_CELL'                  : (['randao_mixes']                       , listOf('Hash', converter = hexIntToken))
+                    , 'ACTIVE_INDEX_ROOTS_CELL'            : (['active_index_roots']                 , listOf('Hash', converter = hexIntToken))
+                    , 'COMPACT_COMMITTEES_ROOTS_CELL'      : (['compact_committees_roots']           , listOf('Hash', converter = hexIntToken))
+                    , 'SLASHINGS_CELL'                     : (['slashings']                          , indexedMapOf(converter = intToken))
+                    , 'PREVIOUS_EPOCH_ATTESTATION_CELL'    : (['previous_epoch_attestations']        , listOf('PendingAttestation', converter = pendingAttestationTerm))
+                    , 'CURRENT_EPOCH_ATTESTATIONS_CELL'    : (['current_epoch_attestations']         , listOf('PendingAttestation', converter = pendingAttestationTerm))
+                    , 'PREVIOUS_CROSSLINKS_CELL'           : (['previous_crosslinks']                , indexedMapOf(converter = crosslinkTerm))
+                    , 'CURRENT_CROSSLINKS_CELL'            : (['current_crosslinks']                 , indexedMapOf(converter = crosslinkTerm))
+                    , 'JUSTIFICATION_BITS_CELL'            : (['justification_bits']                 , lambda inputInt: listOf('Bit', converter = intToken)([i for i in bin(int(inputInt, 16))[2:]]))
+                    , 'PREVIOUS_JUSTIFIED_CHECKPOINT_CELL' : (['previous_justified_checkpoint']      , checkpointTerm)
+                    , 'CURRENT_JUSTIFIED_CHECKPOINT_CELL'  : (['current_justified_checkpoint']       , checkpointTerm)
+                    , 'FINALIZED_CHECKPOINT_CELL'          : (['finalized_checkpoint']               , checkpointTerm)
+                    , 'BLOCKSLOT_CELL'                     : (['latest_block_header', 'slot']        , intToken)
+                    , 'PARENT_ROOT_CELL'                   : (['latest_block_header', 'parent_root'] , hexIntToken)
+                    , 'STATE_ROOT_CELL'                    : (['latest_block_header', 'state_root']  , hexIntToken)
+                    , 'SIGNATURE_CELL'                     : (['latest_block_header', 'signature']   , intToken)
                     }
+
+def getKeyChain(yaml_input, key_chain):
+    output = copy.deepcopy(yaml_input)
+    for key in key_chain:
+        if key in output:
+            output = output[key]
+        else:
+            _fatal('Key not found: ' + key + ' from chain ' + str(key_chain))
+    return output
+
+def gatherKeyChains(yaml_input):
+    if type(yaml_input) is not dict:
+        return []
+    key_chains = []
+    for k in yaml_input.keys():
+        sub_key_chains = gatherKeyChains(yaml_input[k])
+        if len(sub_key_chains) == 0:
+            key_chains.append([k])
+        else:
+            for sub_key_chain in sub_key_chains:
+                key_chains.append([k] + sub_key_chain)
+    return key_chains
 
 def buildInitConfigSubstitution(test_pre_state, key_table = init_cells, skip_keys = []):
     new_key_table = {}
-    used_pre_keys = []
+    used_key_chains = []
     for cell_var in key_table:
         if cell_var in init_config_cells and cell_var not in skip_keys:
-            (pre_key, converter) = init_config_cells[cell_var]
-            if pre_key in test_pre_state:
-                new_key_table[cell_var] = converter(test_pre_state[pre_key])
-                used_pre_keys.append(pre_key)
-            else:
-                printerr('Not in test_pre_state: ' + pre_key)
+            (pre_keys, converter) = init_config_cells[cell_var]
+            new_key_table[cell_var] = converter(getKeyChain(test_pre_state, pre_keys))
+            used_key_chains.append(pre_keys)
         else:
             new_key_table[cell_var] = key_table[cell_var]
-            printerr('Unset configuration variable: ' + cell_var)
-    for pre_key in set(test_pre_state.keys()).difference(set(used_pre_keys)):
-        printerr('Unused pre_key: ' + pre_key)
+            _warning('Unset configuration variable: ' + cell_var)
+    for pre_key in gatherKeyChains(test_pre_state):
+        if pre_key not in used_key_chains:
+            _warning('Unused pre_key: ' + str(pre_key))
     return new_key_table
 
 if __name__ == '__main__':
@@ -126,9 +151,7 @@ if __name__ == '__main__':
 
     for test_case in yaml_test['test_cases']:
         test_title = test_case['description'] + ' from ' + args.input.name
-        printerr('')
-        printerr(test_title)
-        printerr('='.join(['' for i in test_title + ' ']))
+        _notif(test_title)
 
         if 'bls_setting' in test_case and test_case['bls_setting'] > 1:
             _warning('Skipping test with `bls_setting` set to ' + str(test_case['bls_setting']))
@@ -140,12 +163,12 @@ if __name__ == '__main__':
         kast_json = { 'format' : 'KAST' , 'version' : 1.0 , 'term' : init_config }
 
         if 'transfer' in test_case:
-            printerr('Skipping transfer block!')
+            _warning('Skipping transfer block!')
 
         if test_case['post'] is None:
-            printerr('No post condition!')
+            _warning('No post condition!')
         else:
-            printerr('Skipping post block!')
+            _warning('Skipping post block!')
 
         with tempfile.NamedTemporaryFile(mode = 'w') as tempf:
             json.dump(kast_json, tempf)
@@ -154,10 +177,10 @@ if __name__ == '__main__':
             (returnCode, kastPrinted, _) = kast(tempf.name, '--input', 'json', '--output', 'pretty')
             kastPrinted = kastPrinted.strip()
             if fastPrinted != kastPrinted:
-                printerr('Diff: ')
-                for line in difflib.unified_diff(kastPrinted.split('\n'), fastPrinted.split('\n'), fromfile='kast', tofile='fast', lineterm='\n'):
-                    print(line)
                 _warning('kastPrinted and fastPrinted differ!')
+                for line in difflib.unified_diff(kastPrinted.split('\n'), fastPrinted.split('\n'), fromfile='kast', tofile='fast', lineterm='\n'):
+                    sys.stderr.write(line)
+                sys.stderr.flush()
             (returnCode, _, _) = krun(tempf.name, '--term', '--parser', 'cat')
             if returnCode != 0:
                 _fatal('krun returned non-zero exit code: ' + args.input.name + ' ' + test_case['description'])

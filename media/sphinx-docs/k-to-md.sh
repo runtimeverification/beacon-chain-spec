cf76791b1c89d42d37e36cf997e7d203c252cc82
Chris Hathhorn <chathhorn@gmail.com>
2019-10-04 12:21:37 -0500

    Html/md generator fixes and refactoring. (#109)
    * Html/md generator fixes and refactoring.
    
    * Another fix.

--- media/sphinx-docs/k-to-md.sh
+++ media/sphinx-docs/k-to-md.sh
@@ -16,7 +16,7 @@ extract_python() {
         s/\/\*\s*\n\s*def \([a-zA-Z_]*\)(\(.*\)/\n```\n\n#### `\1`\n\nPython reference:\n```python\ndef \1(\2/g
   }' $1
 
-  # Matches multi-line comment beginning with def ...(
+  # Matches multi-line comment beginning with class ...(
   sed -i 's/^\/\*\s*class \([a-zA-Z_]*\)(\(.*\)/\n```\n\n#### `\1`\n\nPython reference:\n```python\nclass \1(\2/g' $FILE
   sed -i '/\/\*\s*$/{
         N
@@ -27,54 +27,81 @@ extract_python() {
   sed -i 's/^\s*\/\*\(.*\)/\n```\n\nPython reference:\n```python\n\1/g' $1
 
   # End multi-line comment by switching to K mode
-  sed -i 's/^\(.*\)\*\/\s*/\1\n```\n\nK semantics:\n```k/g' $FILE
+  sed -i 's/^\(.*\)\*\/\s*$/\1\n```\n\nK semantics:\n```k\n/g' $1
 
 }
 
+insert_title() {
+  TITLE_PRE='1s/^\(.*\)/'
+  TITLE_MID='\n============\n'
+  TITLE_POST='\1/'
+
+  sed -i "$TITLE_PRE$1$TITLE_MID$2$TITLE_POST" $3
+}
+
+title_no_k() {
+  insert_title "$1" '' "$2"
+}
+
+title() {
+  insert_title "$1" '\n```k\n' "$2"
+}
+
+enter_next_k() {
+  sed -i '1s/^\s*\*\//\n```\n\nK semantics:\n```k\n/g' $1
+}
+
+enter_next_python() {
+  sed -i '1s/^\s*\/\*\(.*\)/Python reference:\n```python\n\1/g' $FILE
+}
+
 FILE=beacon-chain.md
-sed -i '1s/^\(.*\)/Beacon Chain\n============\n\1/' $FILE
+title_no_k "Beacon Chain" $FILE
 sed -i '0,/^\s*\/\*/ s/^ *\/\*/\n\n/g' $FILE
 sed -i '0,/^\s*\*\// s/^ *\*\//\n\n```k\n/g' $FILE
 sed -i '0,/^\s*\*/ s/^ *\*\(.*\)/\1/g' $FILE
 sed -i '0,/^\s*\*/ s/^ *\*\(.*\)/\1/g' $FILE
 
-sed -i '1s/^\s*\/\*\(.*\)/Python reference:\n```python\n\1/g' $FILE
-sed -i '1s/^\s*\*\//\n```\n\nK semantics:\n```k/g' $FILE
+enter_next_python $FILE
+enter_next_k $FILE
 
 extract_python $FILE
 
 echo '```' >> $FILE
 
 FILE=hash-tree.md
-sed -i '1s/^\(.*\)/Hash Tree\n============\n\n```k\n\1/' $FILE
-sed -i '1s/^\s*\/\*\(.*\)/Python reference:\n```python\n\1/g' $FILE
-sed -i '1s/^\s*\*\//\n```\n\nK semantics:\n```k/g' $FILE
+title 'Hash Tree' $FILE
+
+enter_next_python $FILE
+enter_next_k $FILE
 
 extract_python $FILE
 
 echo '```' >> $FILE
 
 FILE=config.md
-sed -i '1s/^\(.*\)/Configuration\n============\n\n```k\n\1/' $FILE
-sed -i 's/^\s*\*\//\n```\n\nK semantics:\n```k/g' $FILE
+title 'Configuration' $FILE
+
+enter_next_k $FILE
 
 extract_python $FILE
 
 echo '```' >> $FILE
 
 FILE=types.md
-sed -i '1s/^\(.*\)/Types\n============\n\n```k\n\1/' $FILE
-sed -i '1s/^\s*\*\//\n```\n\nK semantics:\n```k/g' $FILE
+title 'Types' $FILE
+
+enter_next_k $FILE
 
 extract_python $FILE
 
 echo '```' >> $FILE
 
 FILE=constants-mainnet.md
-sed -i '1s/^\(.*\)/Constants (Mainnet)\n============\n\n```k\1/' $FILE
+title 'Constants (Mainnet)' $FILE
 echo '```' >> $FILE
 
 FILE=constants-minimal.md
-sed -i '1s/^\(.*\)/Constants (Minimal)\n============\n\n```k\1/' $FILE
+title 'Constants (Minimal)' $FILE
 echo '```' >> $FILE
 
